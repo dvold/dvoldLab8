8afc45c83b74292fc18368cc17588b1c64e2406a
dvold <2004VDaniel@gmail.conm>
2024-10-25 01:06:59 -0600

    Pass Test Has City?

--- app/src/main/java/com/example/lab8/City.java
+++ app/src/main/java/com/example/lab8/City.java
@@ -17,4 +17,31 @@ public class City {
     String getProvinceName(){
         return this.province;
     }
+
+    //https://www.baeldung.com/java-equals-hashcode-contracts, 2024-10-25
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof City))
+            return false;
+        City other = (City) o;
+        boolean valueEquals = (this.city == null && other.city == null)
+                || (this.city != null && this.city.equals(other.city));
+        boolean storeEquals = (this.province == null && other.province == null)
+                || (this.province != null && this.province.equals(other.province));
+        return valueEquals && storeEquals;
+    }
+
+    @Override
+    public final int hashCode() {
+        int result = 17;
+        if (city != null) {
+            result = 31 * result + city.hashCode();
+        }
+        if (province != null) {
+            result = 31 * result + province.hashCode();
+        }
+        return result;
+    }
 }
